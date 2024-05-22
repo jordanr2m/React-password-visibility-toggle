8cb642a060f806cadf1526cc8078e121b6996928
Jordan J <jordanr2m@gmail.com>
2024-05-22 14:59:28 -0700

    Added css styles.

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -4,25 +4,27 @@ import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 const Login = () => {
   return (
-    <section className='login'>
-      <div className='--card'>
-        <h2>Login</h2>
+    <section className='login --100vh --center-all'>
+      <div className='--card --bg-light'>
+        <h2 className='--color-danger'>Login</h2>
         <form className='--form-control'>
             <input 
                 type='text'
                 placeholder='Username'
+                className='--width-100'
             />
             {/* Must place password input in another div because it has an icon next to it. Place icon in its own div */}
             <div className='password'>
                 <input 
                     type='password'
                     placeholder='Password'
+                    className='--width-100'
                 />
                 <div className='icon'>
                     <AiOutlineEye />
                 </div>
             </div>
-            <button>Login</button>
+            <button className='--btn --btn-danger --btn-block'>Login</button>
             {/* Does button need type="submit" ? Instructor didn't include it */}
         </form>
       </div>
