31cf2e2272ad079bb10f0c84d937f7ca0d62b7a4
Jordan J <jordanr2m@gmail.com>
2024-05-22 15:30:02 -0700

    Added logic for the project.

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Login.css"
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 const Login = () => {
-  return (
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePassword = () => {
+    // Set showPassword = to its opposite
+    setShowPassword(!showPassword);
+  };
+  
+    return (
     <section className='login --100vh --center-all'>
       <div className='--card --bg-light'>
         <h2 className='--color-danger'>Login</h2>
@@ -16,12 +23,17 @@ const Login = () => {
             {/* Must place password input in another div because it has an icon next to it. Place icon in its own div */}
             <div className='password'>
                 <input 
-                    type='password'
+                // If showPassword is true, let type="text", otherwise it is password & will be hidden
+                    type={showPassword ? "text" : "password"}
                     placeholder='Password'
                     className='--width-100'
                 />
-                <div className='icon'>
-                    <AiOutlineEye />
+                <div className='icon' onClick={togglePassword}>
+                    {/* Check if showPassword is true or false & display cooresponding icon */}
+                    {showPassword ? 
+                        <AiOutlineEyeInvisible /> :
+                        <AiOutlineEye />
+                    }
                 </div>
             </div>
             <button className='--btn --btn-danger --btn-block'>Login</button>
